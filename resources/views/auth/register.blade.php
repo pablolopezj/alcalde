b82d90c95863cc052c99de2c266a07982af90e64
Pablo <pilj@virket.com>
2017-08-21 21:50:35 -0500

    Register new user

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,10 +6,23 @@
         <div class="col-12">
             <form class="form-horizontal" method="POST" action="{{ route('register') }}">
                 {{ csrf_field() }}
+                
+                <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                    <label for="name" class="col-md-12 control-label">Nombre</label>
+                    <div class="input-group col-md-12">
+                        <div class="input-group-addon"><i class="fa fa-user" aria-hidden="true"></i></div>
+                        <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" placeholder="Nombre" required autofocus>
+                        @if ($errors->has('name'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('name') }}</strong>
+                             </span>
+                        @endif
+                    </div>
+                </div>
 
                 <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                     <label for="email" class="col-md-12 control-label">Correo electrónico</label>
-                    <div class=" input-group col-md-12">
+                    <div class="input-group col-md-12">
                         <div class="input-group-addon"><i class="fa fa-id-card-o" aria-hidden="true"></i></div>
                         <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="Correo electrónico" required>
                         @if ($errors->has('email'))
